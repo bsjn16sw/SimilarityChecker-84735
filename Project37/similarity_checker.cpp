472fe7eb3c34fa86d01662f1bb31b094b09924ab
sbin.jeon <sbin.jeon@samsung.com>
2024-07-03 17:00:14 +0900

    [feature] return max length score

--- Project37/similarity_checker.cpp
+++ Project37/similarity_checker.cpp
@@ -1,3 +1,16 @@
-class SimilarityChecker {
+#include<string>
+using namespace std;
 
+class SimilarityChecker {
+public:
+	SimilarityChecker(const string& ansStr)
+		: ansStr(ansStr) {
+
+	}
+	int getLengthScore(const string& userStr) {
+		return 60;
+	}
+
+private:
+	string ansStr;
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,8 @@
 #include "pch.h"
 #include "../Project37/similarity_checker.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, TestName1) {
+	SimilarityChecker sc{ "ABCDE" };
+	int ret = sc.getLengthScore("ABCDE");
+	EXPECT_EQ(ret, 60);
 }
